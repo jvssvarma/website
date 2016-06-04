9899c1ea6a1a47e82278960a5dcdc49d77374174
jvssvarma <varma.santosh@outlook.com>
2016-06-04 01:23:01 -0700

    added basic project functions

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,36 @@
+class ProjectsController < ApplicationController
+	before_action :find_project, only: [:show, :edit, :destroy, :update]
+
+	def index
+		@projects = Project.all.order("created_at desc")
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new project_params
+
+		if @project.save
+			redirect_to @project, notice: "Nice Varma! it's saved!"
+		else
+			render 'new', notice: "Oops! Something went wrong!"
+		end
+	end
+
+	def show
+
+	end
+
+	private
+
+	def project_params
+		params.require(:project).permit(:title, :description, :github, :link)
+	end
+
+	def find_project
+		@project = Project.find(params[:id])
+	end
+
+end
