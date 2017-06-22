77f2bbb55a1cb8a3c29334649774b4308f884bb9
jvssvarma <jampana.varma49@gmail.com>
2017-06-22 16:40:37 -0400

    Add medium editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,23 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require medium-editor
+
+<script>
+  var editor = new MediumEditor('.editable', {
+    // options go here
+    allowMultiParagraphSelection: true,
+    buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', 'quote'],
+    diffLeft: 0,
+    diffTop: -10,
+    firstButtonClass: 'medium-editor-button-first',
+    lastButtonClass: 'medium-editor-button-last',
+    relativeContainer: null,
+    standardizeSelectionStart: false,
+    static: false,
+    /* options which only apply when static is true */
+    align: 'center',
+    sticky: false,
+    updateOnEmptySelection: false
+  });
+</script>
